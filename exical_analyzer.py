4a7947fc0f9ab6680bbb0ee23c4ff99ae490c8dc
luyiming <740674785@qq.com>
2018-12-02 16:35:34 +0800

    最终版语法分析器

--- exical_analyzer.py
+++ exical_analyzer.py
@@ -1,46 +1,20 @@
 # 保留字
 reserve_word = {
-    'end': '0',
-    'bool': '1',
-    'char': '2',
-    'class': '3',
-    'double': '4',
-    'else': '5',
-    'false': '6',
-    'if': '7',
-    'int': '8',
-    'private': '9',
-    'protected': '10',
-    'public': '11',
-    'return': '12',
-    'true': '13',
-    'void': '14',
-    'while': '15',
 
 }
 # 界符
 delimiter = {
-    '(': '17',
-    ')': '18',
-    ',': '19',
-    ';': '20',
-    '[': '21',
-    ']': '22',
-    '{': '23',
-    '}': '24',
+    '#': -1,
+    '(': 1,
+    ')': 2,
 }
 # 运算符
 operator = {
-    '-': '25',
-    '*': '26',
-    '/': '27',
-    '+': '28',
-    '<': '29',
-    '<=': '30',
-    '=': '31',
-    '>': '32',
-    '>=': '33',
-    '==': '34',
+    '-': 3,
+    '*': 4,
+    '/': 5,
+    '+': 6,
+    '=': 7
 }
 # 标识符
 identiflier = {}
@@ -68,12 +42,11 @@ def searchReserve(word):
 def judgeChara(ch):
     if ch == ' ' or ch.isdigit() or ch.isalpha() or operator.get(ch) or delimiter.get(ch) or ch == '.':
         return True
-    print("出现错误字符"+ch)
+    print("出现错误字符" + ch)
     return False
 
 
-
-# 单行字符串词法分析
+# 单行字符串词法分析(单独用于词法分析)
 def lexical_analysis(line):
     for m in line:
         if judgeChara(m) == False:
@@ -97,6 +70,7 @@ def lexical_analysis(line):
             ch = line[i]
             while ch.isalpha() or ch.isdigit():
                 word = word + ch
+                # 往下多读一个
                 i = i + 1
                 if i + 1 > length:
                     break
@@ -108,6 +82,7 @@ def lexical_analysis(line):
                 identiflier[word] = '100'
             else:
                 output[word] = searchReserve(word)
+            i = i - 1
 
         # 开头是数字
         elif ch.isdigit():
@@ -116,12 +91,14 @@ def lexical_analysis(line):
             ch = line[i]
             while ch.isdigit() or ch == '.':
                 word = word + ch
+                # 往下多读一个
                 i = i + 1
                 if i + 1 > length:
                     break
                 ch = line[i]
             output[word] = '99'
             num[word] = '99'
+            i = i - 1
 
         # 是界符
         elif delimiter.get(ch):
@@ -138,6 +115,92 @@ def lexical_analysis(line):
                 output[ch] = operator[ch]
 
 
+# 应用于语法分析的词法分析函数，对每一行返回一个字典，记录单词信息
+def getLexicalAnalysisDict(line):
+    line = line + " "
+    Dict = {}
+    wordID = 0
+    for m in line:
+        if judgeChara(m) == False:
+            return False
+    length = len(line)
+    i = 0
+    ch = line[i]
+    while i + 1 < length:  # 最后会多一个换行符
+        if not i == 0:  # 除了第一个字符，每次开始寻找单词时，将ch初始化为' '
+            ch = ' '
+        word = ''
+        while ch == ' ':
+            i = i + 1
+            if i + 1 > length:
+                break
+            ch = line[i]
+        # 开头是字母
+        if ch.isalpha():
+            word = word + ch
+            i = i + 1
+            ch = line[i]
+            if ch.isalpha() or ch.isdigit():
+                while ch.isalpha() or ch.isdigit():
+                    word = word + ch
+                    i = i + 1
+                    if i + 1 > length:
+                        break
+                    ch = line[i]
+            if searchReserve(word) == None:  # 如果无法在保留字中找到，那么为标识符
+                if len(word) >= 6:
+                    word = word[0:6]
+                Dict[wordID] = ['i', 100, 0, word]
+            else:
+                Dict[wordID] = [word, reserve_word[word], 'null', word]
+            i = i - 1
+        # 开头是数字
+        elif ch.isdigit():
+            word = word + ch
+            i = i + 1
+            ch = line[i]
+            while ch.isdigit() or ch == '.':
+                word = word + ch
+                i = i + 1
+                if i + 1 > length:
+                    break
+                ch = line[i]
+            Dict[wordID] = ['i', 99, int(word), 'num']
+            i = i - 1  # 读入无用字符，回退一位
+
+        # 是界符
+        elif delimiter.get(ch):
+            Dict[wordID] = [ch, delimiter[ch], "null", ch]
+
+        # 是运算符
+        elif operator.get(ch):
+            if i + 1 < length and operator.get(line[i + 1:i + 2]):  # 判断是否是两位的运算符
+                ch2 = line[i:i + 2]
+                if operator.get(ch2):
+                    i = i + 1
+                    Dict[wordID] = [ch2, operator[ch2], "null", ch2]
+            else:
+                Dict[wordID] = [ch, operator[ch], "null", ch]
+        wordID = wordID + 1
+    return Dict
+
+
+# 提供给语法分析的词法分析接口：输入文件名，以列表的形式返回词法分析结果
+def get_exical_analyze_list(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+    lines = preprocessing(lines)
+    noError = True
+    wordList = []
+    for line in lines:
+        value = getLexicalAnalysisDict(line)
+        if value == False:
+            noError = False
+            print("错误行：" + line)
+        wordList.append(value)
+    return wordList
+
+
 def main():
     with open('test.txt') as file:
         lines = file.readlines()
@@ -147,18 +210,21 @@ def main():
         print(line)
     for line in lines:
         if lexical_analysis(line) == False:
-            print("错误行："+line)
+            print("错误行：" + line)
     print('---------------------------------------------------')
     print('输出所有单词')
     for key in output:
-        print("( "+key + ':' + output[key]+" )")
+        print("( " + key + ':' + output[key] + " )")
     print('输出所有标识符')
     for key in identiflier:
-        print("( "+key + ':' + identiflier[key]+" )")
+        print("( " + key + ':' + identiflier[key] + " )")
     print('输出所有数字')
     for key in num:
-        print("( "+key + ':' + num[key]+" )")
+        print("( " + key + ':' + num[key] + " )")
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    a = get_exical_analyze_list('test.txt')
+    for v in a:
+        print(v)
--- syntactic_parser.py
+++ syntactic_parser.py
@@ -1,10 +1,16 @@
 import numpy as np
+from exical_analyzer import get_exical_analyze_list
+from exical_analyzer import delimiter
+from exical_analyzer import operator
+from Stack import Stack
+import time
 
 terminalSymbolSet = set()
 nonTerminalSymbolSet = set()
 TSDict = {}
 NTSDict = {}
 transDict = {0: " ", 1: "<", 2: "=", 3: ">"}
+identiflier = {}
 
 
 def loadSymbol(grammers):
@@ -29,6 +35,7 @@ def loadSymbol(grammers):
     for ch in terminalSymbolList:
         TSDict[ch] = num
         num = num + 1
+    TSDict['#'] = num
 
 
 def getFIRSTOP(grammers):
@@ -125,7 +132,7 @@ def displayLASTOP(LASTOP):
 
 def getOperatorPrecedenceMatrix(grammers, FIRSTOP, LASTOP):
     TSsize = len(terminalSymbolSet)
-    opePreMat = np.zeros((TSsize+1, TSsize+1))  # 0:没有关系 1:小于 2：等于 3：大于
+    opePreMat = np.zeros((TSsize + 1, TSsize + 1))  # 0:没有关系 1:小于 2：等于 3：大于
     for line in grammers:
         right = line[3:-1]  # 去掉换行符
         for part in right.split('|'):
@@ -150,6 +157,7 @@ def getOperatorPrecedenceMatrix(grammers, FIRSTOP, LASTOP):
     for i in range(TSsize):
         opePreMat[-1][i] = 1
         opePreMat[i][-1] = 3
+    opePreMat[-1][-1] = 2
     return opePreMat
 
 
@@ -159,16 +167,154 @@ def displayOpePreMat(opePreMat):
     str = "   "
     for ch in TSDict.keys():
         str = str + ch + "  "
-    print(str+"#")
+    print(str)
     for ch in TSDict.keys():
         str = ch + "  "
         for i in range(TSsize):
-            str = str+transDict[opePreMat[TSDict[ch]][i]]+"  "
-        print(str+transDict[opePreMat[TSDict[ch]][-1]])
-    str = "#  "
-    for i in range(TSsize):
-        str = str+transDict[opePreMat[-1][i]]+"  "
-    print(str)
+            str = str + transDict[opePreMat[TSDict[ch]][i]] + "  "
+        print(str + transDict[opePreMat[TSDict[ch]][-1]])
+
+
+def MainProc_Analysis(line_words):
+    my_stack = Stack()
+    temp_stack = Stack()
+    W0 = ['#', -1, 'null', '#']  # 第一个压入'#'
+    my_stack.push(W0)
+    temp_word = W0
+    for wo in line_words:
+        word = line_words[wo]
+        if word[3] in identiflier.keys():
+            word[2] = identiflier[word[3]]
+        print('-------------------------')
+        print('word', wo, line_words[wo])
+        peek_word = my_stack.peek()
+        k = my_stack.size() - 1
+        while k > -1:
+            if my_stack.items[k][1] in [0, 1, 2, 3, 4, 5, 6, 7, 99, 100]:
+                temp_word = my_stack.items[k]
+                break
+            k = k - 1
+        if myOpePreMat[TSDict[temp_word[0]]][TSDict[word[0]]] == 1:
+            my_stack.push(word)
+            print('添加了新的字符', word[0])
+            my_stack.display()
+        elif myOpePreMat[TSDict[temp_word[0]]][TSDict[word[0]]] == 2:
+            if temp_word[0] == '#' and word[0] == '#':
+                if my_stack.size() == 2:
+                    print('value', peek_word[2])
+                    return True
+                else:
+                    return False
+            else:
+                my_stack.push(word)
+                print('添加了新的字符', word[0])
+                my_stack.display()
+        elif myOpePreMat[TSDict[temp_word[0]]][TSDict[word[0]]] == 3:
+            prior_Relation = 3
+            temp_word = my_stack.peek()
+            newN = ['N', -1, 0, '']
+            # 进行规约
+            while prior_Relation == 3:
+                judge = temp_word[1]
+                if judge in [99, 100]:  # 判断栈顶元素是否是标识符或者数字，规约为N
+                    newN[2] = temp_word[2]
+                    newN[1] = 0 - judge  # 将的值取反
+                    my_stack.pop()
+                    my_stack.push(newN)
+                    print("将栈顶的标识符/数字归约为N")
+
+                # 加法归约
+                if judge == 6:  # N1+N2
+                    N2 = my_stack.pop()
+                    Nadd = my_stack.pop()
+                    N1 = my_stack.pop()
+                    if Nadd[1] != 6:
+                        print("加号归约错误")
+                    N1[2] = N1[2] + N2[2]
+                    my_stack.push(N1)
+                    print("加号归约")
+
+                # 减法归约
+                if judge == 3:  # N1-N2
+                    N2 = my_stack.pop()
+                    Nsub = my_stack.pop()
+                    N1 = my_stack.pop()
+                    if Nsub[1] != 3:
+                        print("减号归约错误")
+                    N1[2] = N1[2] - N2[2]
+                    my_stack.push(N1)
+                    print("减号归约")
+
+                # 乘法归约
+                if judge == 4:  # N1*N2
+                    N2 = my_stack.pop()
+                    Nmul = my_stack.pop()
+                    N1 = my_stack.pop()
+                    if Nmul[1] != 4:
+                        print("乘号归约错误")
+                    N1[2] = N1[2] * N2[2]
+                    my_stack.push(N1)
+                    print("乘号归约")
+
+                # 除法归约
+                if judge == 5:  # N1*N2
+                    N2 = my_stack.pop()
+                    Ndiv = my_stack.pop()
+                    N1 = my_stack.pop()
+                    if Ndiv[1] != 5:
+                        print("除号归约错误")
+                    N1[2] = N1[2] / N2[2]
+                    my_stack.push(N1)
+                    print("除号归约")
+
+                # 右括号归约
+                if judge == 2:  # (N)
+                    Nright = my_stack.pop()
+                    N = my_stack.pop()
+                    Nleft = my_stack.pop()
+                    if Nright[1] != 2:
+                        print("括号归约错误")
+                    my_stack.push(N)
+                    print("括号归约")
+
+                # 等号归约
+                if judge == 7:  # N1=N2
+                    N2 = my_stack.pop()
+                    Neq = my_stack.pop()
+                    N1 = my_stack.pop()
+                    if Neq[1] != 7 and not N1[1] in [100, -100]:
+                        print("等号归约错误")
+                    else:
+                        N1[2] = N2[2]
+                        identiflier[N1[3]] = N1[2]
+                        print('identiflier', identiflier)
+                        print('N1', N1)
+                        print('N2', N2)
+                    my_stack.push(N1)
+                    print("等号归约")
+
+                # 更新temp_word
+                k = my_stack.size() - 1
+                while (k > -1):
+                    if my_stack.items[k][1] in [-1, 0, 1, 2, 3, 4, 5, 6, 7, 99, 100]:
+                        temp_word = my_stack.items[k]
+                        break
+                    k = k - 1
+                prior_Relation = myOpePreMat[TSDict[temp_word[0]]][TSDict[word[0]]]
+                my_stack.display()
+            # 规约结束后，添加字符
+            if prior_Relation == 2:
+                if temp_word[0] == '#' and word[0] == '#':
+                    if my_stack.size() == 2:
+                        print('---------------------------------')
+                        print('归约成功')
+                        print('value', my_stack.peek()[2])
+                        return True
+                    else:
+                        return False
+            my_stack.push(word)
+            print('添加了新的字符', word[0])
+            my_stack.display()
 
 
 def main():
@@ -177,12 +323,18 @@ def main():
     loadSymbol(grammers)
     NTSsize = len(nonTerminalSymbolSet)
     TSsize = len(terminalSymbolSet)
+    print(TSDict)
     FIRSTOP = getFIRSTOP(grammers)
     LASTOP = getLASTOP(grammers)
-    displayFIRSTOP(FIRSTOP)
-    displayLASTOP(LASTOP)
-    opePreMat = getOperatorPrecedenceMatrix(grammers, FIRSTOP, LASTOP)
-    displayOpePreMat(opePreMat)
+    # displayFIRSTOP(FIRSTOP)
+    # displayLASTOP(LASTOP)
+    global myOpePreMat
+    myOpePreMat = getOperatorPrecedenceMatrix(grammers, FIRSTOP, LASTOP)
+    displayOpePreMat(myOpePreMat)
+    lines = get_exical_analyze_list('test.txt')
+    for line in lines:
+        MainProc_Analysis(line)
+    print(identiflier)
 
 
 if __name__ == '__main__':
